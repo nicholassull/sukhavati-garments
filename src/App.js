5b111c5ab57446bed11a610054d68d5d56df9b62
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 19:56:29 -0700

    initialize category array and map in App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      
+    }
+  ]
+
   return (
     <div className="categories-container">
-      <div className="category-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
         {/* <img src="" alt="" /> */}
         <div className="category-body">
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
