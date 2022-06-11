9d827a6c701c8238ed4dad2c5b98113f6511c9fc
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 20:12:06 -0700

    add styling for App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './categories.styles.scss'
 
 const App = () => {
 
@@ -22,21 +23,21 @@ const App = () => {
       id: 5,
       title: 'Mens',
     }
-  ]
+  ];
 
   return (
     <div className="categories-container">
       {categories.map(({title}) => (
         <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
       ))}
     </div>
   );
-}
+};
 
 export default App;
