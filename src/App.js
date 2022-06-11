2a7f53e1caca7226e6e81013c250f84b6feca44e
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 20:19:29 -0700

    add images to background-image in App

--- src/App.js
+++ src/App.js
@@ -32,9 +32,11 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
-          <div className="background-image" />
+      {categories.map(({title, id, imageUrl}) => (
+        <div key={id} className="category-container">
+          <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+          }}/>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
