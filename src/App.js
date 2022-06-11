265ec05ca5172d33084450118a9ce686f72fd43e
Nick Sullivan <nicholassull@gmail.com>
2022-06-10 19:50:45 -0700

    add simple site layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
