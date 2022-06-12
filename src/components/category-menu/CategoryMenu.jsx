db03a49b41402c48df5c9504cd848fded5ac41a9
Nick Sullivan <nicholassull@gmail.com>
2022-06-12 13:52:45 -0700

    add open sans font

--- src/components/category-menu/CategoryMenu.jsx
+++ src/components/category-menu/CategoryMenu.jsx
@@ -3,7 +3,7 @@ import './CategoryMenu.styles.scss';
 
 const CategoryMenu = ({categories}) => {
   return (
-  <div className="categories-container">
+  <div className="menu-container">
     {categories.map((category) => (
       <CategoryItem key={category.id} category={category}/>
     ))}
