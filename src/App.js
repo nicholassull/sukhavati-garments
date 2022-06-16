403375e17f91fe91b085b7aca323ce0559d8ce9a
Nick Sullivan <nicholassull@gmail.com>
2022-06-15 18:36:31 -0700

    add Navigation as a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/routes/home/Home.route';
+import Navigation from './components/navigation/Navigation';
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        {/* The index attribute indicates that this component is considered the base outlet component & will be rendered automatically */}
+        <Route index element={<Home />} />
+        <Route path='shop' element= {<Shop />} />
+      </Route>
     </Routes>
   );
 };
