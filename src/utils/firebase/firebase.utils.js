49ecf53ef3515e19417fa4f343d124fc58950c0c
Nick Sullivan <nicholassull@gmail.com>
2022-06-24 15:44:01 -0700

    Update reference to API key and add button to sign in with Google

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,7 @@ import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from
 
 const firebaseConfig = {
 
-  apiKey: process.env.REACT_APP_FIREBASE_AI_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 
