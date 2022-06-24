37cc78c062cc01c8eb83974cd2362b167e47f6fa
Nick Sullivan <nicholassull@gmail.com>
2022-06-24 15:23:18 -0700

    Add firebase config and initialization

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+//Creates an app instance based off config
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: process.env.REACT_APP_FIREBASE_AI_KEY,
+
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+};
+
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
